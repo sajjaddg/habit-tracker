adecd8dcd7cf990aa314c707e77f3bd153d74faa
sajjaddg <sajjaddg@gmail.com>
2025-04-07 00:03:17 +0330

    [ADD] : Added ModeToggle for change theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import { ModeToggle } from "@/components/mode-toggle"
+import { Button } from "@/components/ui/button"
+import { Plus } from "lucide-react"
+
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20"></div>
+    <div className="flex flex-col p-4">
+      <div className="flex items-center justify-between">
+        <div className="flex flex-col gap-px">
+          <h1>My challenges</h1>
+          <span>Wed, 18</span>
+        </div>
+        <ModeToggle />
+        <Button variant="ghost" size="icon">
+          <Plus className="h-5 w-5" />
+        </Button>
+      </div>
+    </div>
   )
 }
---
+++ components/mode-toggle.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import { Moon, Sun } from "lucide-react"
+import { useTheme } from "next-themes"
+import { Button } from "@/components/ui/button"
+
+export function ModeToggle() {
+  const { setTheme, theme } = useTheme()
+  const toggleSwitch = (theme: string) => document.startViewTransition(() => setTheme(theme))
+
+  return (
+    <Button variant="outline" size="icon" onClick={() => toggleSwitch(theme === "dark" ? "light" : "dark")}>
+      <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+      <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  )
+}
